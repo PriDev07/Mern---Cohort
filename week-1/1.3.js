3b3bb95910e6d4a5dba87ea545d9ae4228f09fa5
PriDev07 <lohaniprince72@gmail.com>
2024-03-28 21:49:48 +0530

    first commit

--- week-1/1.3.js
+++ week-1/1.3.js
@@ -1,37 +1,45 @@
-// String length
-const str = "Jai shree ram";
-function getLength(str){
-    console.log("original string : ", str);
-    console.log("String size : ", str.length);
-}
-getLength(str);
+// // String length
+// const str = "Jai shree ram";
+// function getLength(str){
+//     console.log("original string : ", str);
+//     console.log("String size : ", str.length);
+// }
+// getLength(str);
 
-// find index of 
-function findindexof(str){
-    console.log(str.indexOf("ram"));
-}
-findindexof(str);
+// // find index of 
+// function findindexof(str){
+//     console.log(str.indexOf("ram"));
+// }
+// findindexof(str);
 
-// slice : give only between of index
-function getslice(str){
-    console.log(str.slice(0,9)); // output of str from 0 to 8(not include 9)
-}
-getslice(str);
+// // slice : give only between of index
+// function getslice(str){
+//     console.log(str.slice(0,9)); // output of str from 0 to 8(not include 9)
+// }
+// getslice(str);
 
-// replace : replace string part with some other string 
-function strreplace(str){
-    console.log(str.replace("ram","Hanuman"))
-}
-strreplace(str);
+// // replace : replace string part with some other string 
+// function strreplace(str){
+//     console.log(str.replace("ram","Hanuman"))
+// }
+// strreplace(str);
 
-//split: split string to array index based on desired input
-function strsplit(str){
-    console.log(str.split(" "));
-}
-strsplit(str);
+// //split: split string to array index based on desired input
+// function strsplit(str){
+//     console.log(str.split(" "));
+// }
+// strsplit(str);
 
-// trim : trimmed out extra spaces from beginning and ending of string
-function strtrim(str){
-    console.log(str.trim());
-}
-strtrim(str);
+// // trim : trimmed out extra spaces from beginning and ending of string
+// function strtrim(str){
+//     console.log(str.trim());
+// }
+// strtrim(str);
+
+// Numbers
+// var a="33koti"
+// // parseInt : convert string to integer remove all chars, parseFloat: same as parseInt
+// function checkparint(a){
+//     console.log(parseInt(a));
+// }
+// checkparint(a);
