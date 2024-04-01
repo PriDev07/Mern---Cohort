60222f322485f99db0ff6615fbe8ae774ea8a386
PriDev07 <lohaniprince72@gmail.com>
2024-04-01 18:14:17 +0530

    init

---
+++ week-2/2.3.js
@@ -0,0 +1,9 @@
+// Bash and Terminal Commands
+// 1. pwd : print working directory (used to print path of the current working directory)
+
+// 2. cd : command directory(to naviagte to given directory which i will give using path after cd)
+// ex : cd priyanshulohani/users/(this will go to users folder inside priyanshulohani folder)
+
+// 3. ls : list (to show list of all files and folder of your current working directory)
+
+//
