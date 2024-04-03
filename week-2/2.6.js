eb5024377b94c8c26afbe7cfffba9ad7a1f352ec
PriDev07 <lohaniprince72@gmail.com>
2024-04-03 19:11:08 +0530

    init

---
+++ week-2/2.6.js
@@ -0,0 +1,13 @@
+// map, filter,arrow functions
+
+// function sum(a,b){
+//     return a+b;
+// }
+// const sum = (a,b) => {
+//     return a+b;
+// }
+// // both above are similar 
+// let a = sum(3,4);
+// console.log(a);
+
+// given an array, give me a new array in which every value is multiplied by 2
