6e71f00708e6d74f25143d3fdca53dfd46e77f66
PriDev07 <lohaniprince72@gmail.com>
2024-04-03 19:18:05 +0530

    init

--- week-2/2.6.js
+++ week-2/2.6.js
@@ -10,4 +10,13 @@
 // let a = sum(3,4);
 // console.log(a);
 
-// given an array, give me a new array in which every value is multiplied by 2
+
+// Map
+// given an array, give me a new array in which every value is multiplied by 2 by using map
+
+const input = [1,2,3,4,5];
+function transform(i){
+    return i*2;
+}
+const ans = input.map(transform);
+console.log(ans);
