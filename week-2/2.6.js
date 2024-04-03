0aeea23a67f56f2b271199b6f510a8d903397f7c
PriDev07 <lohaniprince72@gmail.com>
2024-04-03 19:25:44 +0530

    init

--- week-2/2.6.js
+++ week-2/2.6.js
@@ -11,12 +11,26 @@
 // console.log(a);
 
 
-// Map
-// given an array, give me a new array in which every value is multiplied by 2 by using map
+// // Map
+// // given an array, give me a new array in which every value is multiplied by 2 by using map
 
-const input = [1,2,3,4,5];
-function transform(i){
-    return i*2;
+// const input = [1,2,3,4,5];
+// function transform(i){
+//     return i*2;
+// }
+// const ans = input.map(transform);
+// console.log(ans);
+
+
+//Filtering
+//Give an input array, give me back all input values from it
+const arr= [1,2,3,4,5];
+function filterlogic(n){
+    if(n%2==0){
+        return true;
+    } else{
+        return false;
+    }
 }
-const ans = input.map(transform);
+const ans = arr.filter(filterlogic);
 console.log(ans);
